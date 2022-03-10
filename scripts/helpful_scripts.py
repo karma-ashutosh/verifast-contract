2857c316d64da9ac00f299a2d142945f60493f83
yogesh.kg <ykg2992@gmail.com>
2022-03-10 11:57:26 +0530

    separated development and forked development chains

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,12 +1,18 @@
 from brownie import accounts, network, config
 
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "mainnet-fork"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+
 
 def get_account(index=None, id=None):
     if index:
         return accounts[index]
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     if id:
         return accounts.load(id)
