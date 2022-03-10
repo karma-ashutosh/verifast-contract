8a18a101676ceae008f16e739d945b67c62b49d2
yogesh.kg <ykg2992@gmail.com>
2022-03-10 12:33:04 +0530

    get contract functionality added, specially required for general networks

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, Contract
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache", "ganache-local"]
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
@@ -17,3 +17,31 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_contract_type = {}
+
+
+def get_contract(contract_name):
+    """
+    This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that mock contract.
+    """
+    contract_type = contract_to_contract_type[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+# add contracts as they are needed.
+def deploy_mocks():
+    account = get_account()
+    print("Mocks Deployed!")
